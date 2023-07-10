5443c30bd7122a06def5825185b884c052602943
kimihito <tatsurotamashiro@gmail.com>
2023-07-10 22:45:52 +0900

    simplify

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/chromedp/cdproto/dom"
-	"github.com/chromedp/chromedp"
 	"github.com/geziyor/geziyor"
 	"github.com/geziyor/geziyor/client"
 )
@@ -14,21 +11,7 @@ import (
 func main() {
 	geziyor.NewGeziyor(&geziyor.Options{
 		StartRequestsFunc: func(g *geziyor.Geziyor) {
-			req, _ := client.NewRequest("GET", "https://www.e-uchina.net/jukyo?bukken_type=3&sort=senyu_space_large", nil)
-			req.Rendered = true
-			req.Actions = []chromedp.Action{
-				chromedp.Navigate("https://www.e-uchina.net/jukyo?bukken_type=3&sort=senyu_space_large"),
-				chromedp.WaitReady("div.search-result"),
-				chromedp.ActionFunc(func(ctx context.Context) error {
-					node, err := dom.GetDocument().Do(ctx)
-					if err != nil {
-						return err
-					}
-					_, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
-					return err
-				}),
-			}
-			g.Do(req, g.Opt.ParseFunc)
+			g.GetRendered("https://www.e-uchina.net/jukyo?bukken_type=3&sort=senyu_space_large", g.Opt.ParseFunc)
 		},
 		ParseFunc: func(g *geziyor.Geziyor, r *client.Response) {
 			r.HTMLDoc.Find("div.search-result-item").Each(func(_ int, s *goquery.Selection) {
